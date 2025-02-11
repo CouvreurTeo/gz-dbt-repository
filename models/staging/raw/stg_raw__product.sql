793b1a9e19973e34b3de332d5d0da247d88ee2bd
Teo Couvreur <teo.couvreur4@gmail.com>
2025-02-11 10:48:16 +0000

    Chall 4 V10

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -1,19 +1,3 @@
-with 
-
-source as (
-
-    select * from {{ source('raw', 'product') }}
-
-),
-
-renamed as (
-
-    select
-        products_id,
-        purchse_price
-
-    from source
-
-)
-
-select * from renamed
+SELECT *, 
+    SAFE_CAST(purchse_price AS FLOAT64) AS purchase_price
+FROM {{ ref('stg_raw__product') }}
